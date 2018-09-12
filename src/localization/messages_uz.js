7db3691921f297cfac3f8ce4b8f38865f50a68a0
davonrahimov92 <davronrahimov92@gmail.com>
2018-09-12 12:20:02 +0500

    Create messages_uz.js

---
+++ src/localization/messages_uz.js
@@ -0,0 +1,23 @@
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * Locale: UZ (Uzbek; o'zbek tili)
+ */
+$.extend( $.validator.messages, {
+    required: "Ushbu maydon ni to'ldirish shart",
+    remote: "Iltimos, to'gri belgi kiriting.",
+    email: "Iltimos, elektron manzilni to'gri kiriting.",
+    url: "Iltimos, URL ni to'gri kiriting.",
+    date: "Iltimos, to'gri sana kiriting.",
+    dateISO: "Iltimos, ISO formatidagi sanani to'gri kiriting.",
+    number: "Iltimos, sanani kiriting.",
+    digits: "Iltimos, faqat raqam kiritilsin",
+    creditcard: "Iltimos, kredit kartaning raqamini to'g'ri kiriting.",
+    equalTo: "Iltimos, kiritilgan belgilarni takroran kiriting.",
+    extension: "Iltimos, to'g'ri kengaytmali fayl kiriting.",
+    maxlength: $.validator.format("Iltimos, {0} dan ko'p bo'lmagan simvol kiriting."),
+    minlength: $.validator.format("Iltimos, {0} dan kam bo'lmagan simvol kiriting."),
+    rangelength: $.validator.format("Iltimos, uzunligi {0} dan {1}  gacha bo'lgan simvollarni kiriting."),
+    range: $.validator.format("Iltimos, {0} dan {1} gacha bo'lgan son kiriting."),
+    max: $.validator.format("Iltimos, kichik son yoki {0} ga teng bo'lgan son kiriting."),
+    min: $.validator.format("Iltimos, {0}. dan katta yoki teng son kiriting.")
+});
